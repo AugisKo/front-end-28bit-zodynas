d7544604a3e0a4fb97389da0a33f4324fac6e2b1
Eugenijus <augisk@gmail.com>
2021-07-08 20:02:04 +0300

    js error

--- js/components/Zodynas.js
+++ js/components/Zodynas.js
@@ -1,47 +1,14 @@
 class Zodynas {
-  constructor(IDselector) {
-    this.IDselector = IDselector;
-
+  constructor() {
     this.DOM = null;
-    this.listDOM = null;
 
     this.addFormDOM = null;
-    this.newMessageDOM = null;
-    this.buttonSaveDOM = null;
-    this.buttonEraseDOM = null;
-    this.buttonEditDOM = null;
-
-    this.updateFormDOM = null;
-    this.buttonUpdateDOM = null;
-    this.buttonCancelDOM = null;
-
-    this.localStorageIDcount = 'zodynasID';
-    this.localStorageTodosKey = 'zodynasList';
-    this.latestUsedID =
-      JSON.parse(localStorage.getItem(this.localStorageIDcount)) || 0;
-    this.messages =
-      JSON.parse(localStorage.getItem(this.localStorageZodynasKey)) || [];
-
-    this.currentlyEditableTaskID = 0;
 
     this.init();
   }
 
   init() {
-    if (!this.isValidSelector()) {
-      return false;
-    }
-
-    this.DOM = document.getElementById(this.IDselector);
-    if (!this.DOM) {
-      console.error('ERROR: nerasta vieta, pagal duota selector');
-      return false;
-    }
-    this.DOM.classList.add('zodynas');
-
     this.render();
-    this.renderList();
-    this.addEvents();
   }
 
   isValidSelector() {
@@ -69,26 +36,26 @@ class Zodynas {
     </form>`;
   }
 
+  tableRow() {
+    return ` <tr>
+                <td>${angliskasZodis}</td>
+                <td>${LietuviskasZodis}</td>
+                <td class="btn">
+                    <button name="edit" class="fa fa-pencil"></button>
+                    <button name="delete" class="fa fa-trash"></button>
+                </td>
+            </tr>`;
+  }
+
   render() {
     let HTML = '';
-    this.DOM.insertAdjacentHTML('afterend', HTML);
     HTML += this.generateAddForm();
-    /*  HTML += this.generateUpdateForm();
-    HTML += this.generateList();*/
-    this.DOM.innerHTML = HTML;
 
-    this.listDOM = this.DOM.querySelector('.list');
-
+    this.DOM.insertAdjacentHTML('afterend', HTML);
     this.addFormDOM = document.getElementById('add_zodis');
-    this.newMessageDOM = document.getElementById('new_text');
-    this.newBorderColorDOM = document.getElementById('new_border_color');
-    this.buttonSaveDOM = document.getElementById('save_button');
-
-    this.updateFormDOM = document.getElementById('update_task');
-    this.updateMessageDOM = document.getElementById('updated_text');
-    this.updateBorderColorDOM = document.getElementById('updated_border_color');
-    this.buttonUpdateDOM = document.getElementById('update_button');
-    this.buttonCancelDOM = document.getElementById('cancel_button');
+    var newRow = document.getElementById('zodyno_body').insertRow();
+    newRow.innerHTML = this.tableRow;
+    HTML += this.newRow;
   }
 }
 
---
+++ read.md
@@ -0,0 +1,68 @@
+ <!---
+    <form class="ivesties_forma">
+        <div class="ang">
+            <lable for="eng">Anglų</lable>
+            <input type="text" id="eng" name="text">
+        </div>
+        <div class="liet">
+            <lable for="liet">Lietuvių</lable>
+            <input type="text" id="liet" name="text">
+        </div>
+        <div class="btn">
+            <button class="fa fa-floppy-o" name="save" type="submit" value="Save"></button>
+            <button class="fa fa-eraser" name="reset" type="submit" value="Reset"></button>
+        </div>
+    </form>
+      <div class="isvestis">
+        <table>
+            <tr>
+                <td>Angliskas zodis</td>
+                <td>Lietuviskas zodis</td>
+                <td class="btn">
+                    <button name="edit" class="fa fa-pencil"></button>
+                    <button name="delete" class="fa fa-trash"></button>
+                </td>
+            </tr>
+            <tr>
+                <td>Angliskas zodis</td>
+                <td>Lietuviskas zodis</td>
+                <td class="btn">
+                    <button name="edit" class="fa fa-pencil"></button>
+                    <button name="delete" class="fa fa-trash"></button>
+                </td>
+            </tr>
+            <tr>
+                <td>Angliskas zodis</td>
+                <td>Lietuviskas zodis</td>
+                <td class="btn">
+                    <button name="edit" class="fa fa-pencil"></button>
+                    <button name="delete" class="fa fa-trash"></button>
+                </td>
+            </tr>
+            <tr>
+                <td>Angliskas zodis</td>
+                <td>Lietuviskas zodis</td>
+                <td class="btn">
+                    <button name="edit" class="fa fa-pencil"></button>
+                    <button name="delete" class="fa fa-trash"></button>
+                </td>
+            </tr>
+            <tr>
+                <td>Angliskas zodis</td>
+                <td>Lietuviskas zodis</td>
+                <td class="btn">
+                    <button name="edit" class="fa fa-pencil"></button>
+                    <button name="delete" class="fa fa-trash"></button>
+                </td>
+            </tr>
+            <tr>
+                <td>Angliskas zodis</td>
+                <td>Lietuviskas zodis</td>
+                <td class="btn">
+                    <button name="edit" class="fa fa-pencil"></button>
+                    <button name="delete" class="fa fa-trash"></button>
+                </td>
+            </tr>
+        </table>
+    </div>
+    --->
